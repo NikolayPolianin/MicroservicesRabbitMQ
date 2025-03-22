4b1285d7dfc9fa929cdb746791bf2cca108f6b04
NikolayPolianin <nikolai@MacBookPro.local>
2025-03-23 02:11:04 +0300

    add connect and queue

--- ServiceTwo/main.go
+++ ServiceTwo/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"context"
 	"log"
-	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
-)
+) 
+
 func main(){
 	conn,err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil{
 		log.Fatalf("unable to open connect to RabbitMQ server. Error: %s",err)
 	}
 	defer conn.Close()
-
 	ch,err := conn.Channel()
 	if err != nil{
-		log.Fatalf("failed to open channel. Error: %s", err)
+		log.Fatalf("failed to open channel. Error: %s",err)
 	}
 	defer ch.Close()
 	q,err := ch.QueueDeclare(
@@ -25,25 +23,28 @@ func main(){
 		false,
 		false,
 		false,
+		nil,)
+	if err != nil{
+		log.Fatalf("failed to create queue. Error: %s",err)
+	}
+	messages,err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false, 
+		false, 
 		nil,
 	)
 	if err != nil{
-		log.Fatalf("failed to create queue. Error: %s", err)
+		log.Fatalf("failed to create queue. Error: %s",err)
 	}
-	ctx,cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-	defer cancel()
+	var forever chan struct{}
 
-	body := "hello world"
-	err = ch.PublishWithContext(ctx,
-		"",
-		q.Name,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType:"text/plain",
-			Body: []byte(body)})
-	if err != nil{
-		log.Fatalf("failed publish message Error: %s",err)
-	}
-	
+	go func(){
+	for message := range messages{
+		log.Printf("received message: %s",message.Body)
+	}}()
+	log.Printf("Wait Message")
+	<- forever
 }
